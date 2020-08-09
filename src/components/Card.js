3b4dbacb6dd297d380745bf2e255b0d85ef5a625
MARTAKOZIOL <martaannakoziol@hotmail.com>
2020-08-09 13:53:45 +0100

    Add basic cards and images

---
+++ src/components/Card.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const styles = muiBaseTheme => ({
+  card: {
+    maxWidth: 300,
+    margin: 'auto',
+    transition: '0.5s',
+    boxShadow: '0 8px 40px -12px rgba(0,0,0,0.3)',
+    '&:hover': {
+      boxShadow: '0 16px 70px -12.125px rgba(0,0,0,0.3)'
+    }
+  },
+  media: {
+    paddingTop: '56.25%'
+  },
+  content: {
+    textAlign: 'left',
+    padding: muiBaseTheme.spacing.unit * 3
+  },
+  divider: {
+    margin: `${muiBaseTheme.spacing.unit * 3}px 0`
+  },
+  heading: {
+    fontWeight: 'bold'
+  },
+  subheading: {
+    lineHeight: 1.8
+  },
+  avatar: {
+    display: 'inline-block',
+    border: '2px solid white',
+    '&:not(:first-of-type)': {
+      marginLeft: -muiBaseTheme.spacing.unit
+    }
+  }
+});
+
+function OutlinedCard({ classes, project, description, imagePath }) {
+  return (
+    <Card className={classes.card}>
+      <CardMedia className={classes.media} image={imagePath} />
+      <CardContent className={classes.content}>
+        <Typography
+          className={'MuiTypography--heading'}
+          variant={'h6'}
+          gutterBottom
+        >
+          {project}
+        </Typography>
+        <Typography className={'MuiTypography--subheading'} variant={'caption'}>
+          {description}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+const WrappedApp = withStyles(styles)(OutlinedCard);
+
+export default WrappedApp;
--- src/components/Container.js
+++ src/components/Container.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Card from './Card';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -15,23 +16,42 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function FullWidthGrid() {
+  const interestsList = [
+    {
+      title: 'Motorbikes',
+      description: ' 1/9 scale Motorcycles',
+      imagePath: '/images/IMG_20200609_124608243_MP.jpg'
+    },
+    {
+      title: 'Ships',
+      description: '1/89 scale Hermione laFayette 1780',
+      imagePath: '/images/IMG_20200516_110930235.jpg'
+    },
+    {
+      title: 'DollsHouse Miniatures',
+      description: '1/12 scale furniture projects',
+      imagePath: '/images/IMG_20200112_191247.jpg'
+    },
+    {
+      title: 'Mounteneering',
+      description: 'Annual Trip to Sniezka',
+      imagePath: '/images/IMG_20190912_160139.jpg'
+    }
+  ];
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>Motorbikes</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>Ships</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>DollsHouse Miniatures</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>Piano</Paper>
-        </Grid>
+        {interestsList.map((interest, key) => (
+          <Grid key={key} item xs={12} sm={6} md={6} lg={6}>
+            <Card
+              project={interest.title}
+              description={interest.description}
+              imagePath={interest.imagePath}
+            ></Card>
+          </Grid>
+        ))}
       </Grid>
     </div>
   );
