fcf91f93849f68898256e6f138c267c42da4a723
MARTAKOZIOL <martaannakoziol@hotmail.com>
2020-08-11 16:57:03 +0100

    Add react spring

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import { Card, Image } from 'semantic-ui-react';
+import { useSpring, animated } from 'react-spring';
+
+const calc = (x, y) => [
+  -(y - window.innerHeight / 2) / 20,
+  (x - window.innerWidth / 2) / 20,
+  1.1
+];
+const trans = (x, y, s) => ` scale(${s})`;
 
 function InterestCard({ project, description, imagePath }) {
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 5, tension: 350, friction: 40 }
+  }));
   return (
-    <Card
-      raised
-      style={{
-        margin: '30px',
-        borderRadius: '30px',
-        overflow: 'hidden',
-        background: '#d9e6e1'
-      }}
+    <animated.div
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+      onMouseLeave={() => set({ xys: [0, 0, 1] })}
+      style={{ transform: props.xys.interpolate(trans) }}
     >
-      <Image src={imagePath} wrapped ui={false} />
-      <Card.Content>
-        <Card.Header>{project}</Card.Header>
-        <Card.Description>{description}</Card.Description>
-      </Card.Content>
-    </Card>
+      <Card
+        raised
+        style={{
+          margin: '30px',
+          borderRadius: '30px',
+          overflow: 'hidden',
+          background: '#d9e6e1'
+        }}
+      >
+        <Image src={imagePath} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{project}</Card.Header>
+          <Card.Description>{description}</Card.Description>
+        </Card.Content>
+      </Card>
+    </animated.div>
   );
 }
 
