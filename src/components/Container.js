61c483c139bac05223886f44277e24db7140bb6c
MARTAKOZIOL <martaannakoziol@hotmail.com>
2020-08-11 12:33:39 +0100

    Replace Material-ui Grid with Semantic-ui and uninstall Material-ui

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,49 +1,30 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
+import { Grid } from 'semantic-ui-react';
 import Card from './Card';
 import { Link } from 'react-router-dom';
 import { INTERESTS } from '../data';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setActiveTab } from '../actions';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    marginTop: '64px'
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary
-  }
-}));
-
 export default function FullWidthGrid() {
-  const classes = useStyles();
   const dispatch = useDispatch();
-  const activeItem = useSelector(state => state.setActiveTab);
 
   return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        {INTERESTS.filter(item => item.title !== 'Home').map(
-          (interest, key) => (
-            <Grid key={key} item xs={12} sm={6} md={6} lg={6}>
-              <Link
-                to={`/${interest.title}`}
-                onClick={() => dispatch(setActiveTab(interest.title))}
-              >
-                <Card
-                  project={interest.title}
-                  description={interest.description}
-                  imagePath={interest.imagePath}
-                ></Card>
-              </Link>
-            </Grid>
-          )
-        )}
-      </Grid>
-    </div>
+    <Grid container stackable stretched columns={2}>
+      {INTERESTS.filter(item => item.title !== 'Home').map((interest, key) => (
+        <Grid.Column mobile={16} tablet={8} computer={4}>
+          <Link
+            to={`/${interest.title}`}
+            onClick={() => dispatch(setActiveTab(interest.title))}
+          >
+            <Card
+              project={interest.title}
+              description={interest.description}
+              imagePath={interest.imagePath}
+            ></Card>
+          </Link>
+        </Grid.Column>
+      ))}
+    </Grid>
   );
 }
