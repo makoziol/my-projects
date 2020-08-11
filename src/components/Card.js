9689e9b46234e7847903b437217b82053b9c3624
MARTAKOZIOL <martaannakoziol@hotmail.com>
2020-08-11 15:39:34 +0100

    Round top of the card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,10 @@ import { Card, Image } from 'semantic-ui-react';
 
 function InterestCard({ project, description, imagePath }) {
   return (
-    <Card raised style={{ margin: '10px', borderRadius: '30px' }}>
+    <Card
+      raised
+      style={{ margin: '30px', borderRadius: '30px', overflow: 'hidden' }}
+    >
       <Image src={imagePath} wrapped ui={false} />
       <Card.Content>
         <Card.Header>{project}</Card.Header>
