2914e4826ba03ba3b50b138d672e74a1db24870d
MARTAKOZIOL <martaannakoziol@hotmail.com>
2020-08-12 15:49:44 +0100

    Filter cards based on selected search

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,29 +2,32 @@ import React from 'react';
 import Card from './Card';
 import { Link } from 'react-router-dom';
 import { INTERESTS } from '../data';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { setActiveTab } from '../actions';
 
 export default function FullWidthGrid() {
   const dispatch = useDispatch();
+  const value = useSelector(state => state.setSearchedValue);
 
   return (
     <div style={{ display: 'flex', width: '100%', overflow: 'auto' }}>
-      {INTERESTS.filter(item => item.title !== 'Home').map(
-        (interest, index) => (
-          <Link
-            key={index}
-            to={`/${interest.title}`}
-            onClick={() => dispatch(setActiveTab(interest.title))}
-          >
-            <Card
-              project={interest.title}
-              description={interest.description}
-              image={interest.image}
-            ></Card>
-          </Link>
-        )
-      )}
+      {INTERESTS.filter(item => {
+        if (value) {
+          return item.title !== 'Home' && item.title === value;
+        } else return item.title !== 'Home';
+      }).map((interest, index) => (
+        <Link
+          key={index}
+          to={`/${interest.title}`}
+          onClick={() => dispatch(setActiveTab(interest.title))}
+        >
+          <Card
+            project={interest.title}
+            description={interest.description}
+            image={interest.image}
+          ></Card>
+        </Link>
+      ))}
     </div>
   );
 }
