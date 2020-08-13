8c5425480834caefd891d2e066ad5cacc8752661
MARTAKOZIOL <martaannakoziol@hotmail.com>
2020-08-13 17:51:30 +0100

    Assign photos to sections and delete unused photos

--- src/data.js
+++ src/data.js
@@ -14,11 +14,6 @@ export const INTERESTS = [
     description: '1/89 scale Hermione laFayette 1780',
     image: '/images/IMG_20200516_110930235.jpg'
   },
-  // {
-  //   title: 'Miniatures',
-  //   description: '1/12 scale furniture projects',
-  //   image: '/images/IMG_20200112_191247.jpg'
-  // },
   {
     title: 'Mountaineering',
     description: 'Annual Trip to Sniezka',
@@ -34,4 +29,48 @@ export const INTERESTS = [
     description: 'Trips to Caravans by the Sea',
     image: '/images/IMG_20190706_194908.jpg'
   }
+];
+
+export const TT_PHOTOS = [
+  '/images/IMG_20190528_100116.jpg',
+  '/images/IMG_20190528_085706.jpg',
+  '/images/IMG_20190528_110104.jpg',
+  '/images/IMG_20190528_135558.jpg',
+  '/images/IMG_20190528_140449.jpg',
+  '/images/IMG_20190528_173038.jpg'
+];
+
+export const CRAIG_TARA_PHOTOS = [
+  '/images/IMG_20190706_194856.jpg',
+  'images/IMG_20190706_194914.jpg',
+  '/images/IMG_20190706_195653.jpg',
+  '/images/IMG_20190706_200747.jpg',
+  '/images/IMG_20190706_200756.jpg',
+  '/images/IMG_20190707_103044.jpg'
+];
+
+export const KARPACZ_PHOTOS = [
+  '/images/IMG_20190911_134235.jpg',
+  '/images/IMG_20190911_144407.jpg',
+  '/images/IMG_20190912_122633.jpg',
+  '/images/IMG_20190912_123358.jpg',
+  '/images/IMG_20190912_152731.jpg',
+  '/images/IMG_20190912_152735.jpg',
+  '/images/IMG_20190912_160139.jpg'
+];
+
+export const SHIP_PHOTOS = [
+  '/images/IMG_20200519_101233413.jpg',
+  '/images/IMG_20200512_130605405.jpg',
+  '/images/IMG_20200420_195037476.jpg',
+  '/images/IMG_20200420_205318362.jpg'
+];
+
+export const MOTORCYCLE_KITS_PHOTOS = [
+  '/images/IMG_20200706_084757060_MP.jpg',
+  '/images/IMG_20200618_125642032_MP.jpg',
+  '/images/IMG_20200614_165807268_MP.jpg',
+  '/images/IMG_20200609_124602704_MP.jpg',
+  '/images/IMG_20200531_131142794_MP.jpg',
+  '/images/IMG_20200523_125802843.jpg'
 ];
