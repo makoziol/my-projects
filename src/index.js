32994c1cc05889c5f896271e85a82c36333b4181
MARTAKOZIOL <martaannakoziol@hotmail.com>
2020-08-15 15:27:43 +0100

    Add NotFound component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import App from './App';
 import Ships from './components/Ships';
 import Motorcycles from './components/Motorcycles';
 import Mounteneering from './components/Mounteneering';
 import TouristTrophy from './components/TouristTrophy';
 import CraigTara from './components/CraigTara';
+import NotFound from './components/NotFound';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import allReducers from './reducers/index';
@@ -18,13 +19,16 @@ let store = createStore(allReducers);
 const routing = (
   <Provider store={store}>
     <Router>
-      <Route exact path="/" component={App} />
-      <Route path="/home" component={App} />
-      <Route path="/ships" component={Ships} />
-      <Route path="/mountaineering" component={Mounteneering} />
-      <Route path="/motorcycles" component={Motorcycles} />
-      <Route path="/Tourist Trophy" component={TouristTrophy} />
-      <Route path="/Craig Tara" component={CraigTara} />
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/home" component={App} />
+        <Route path="/ships" component={Ships} />
+        <Route path="/mountaineering" component={Mounteneering} />
+        <Route path="/motorcycles" component={Motorcycles} />
+        <Route path="/Tourist Trophy" component={TouristTrophy} />
+        <Route path="/Craig Tara" component={CraigTara} />
+        <Route path="*" component={NotFound} />
+      </Switch>
     </Router>
   </Provider>
 );
