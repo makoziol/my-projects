0da32a6344fb20ce0581a97835fd585381a5d0af
MARTAKOZIOL <martaannakoziol@hotmail.com>
2020-08-15 14:41:21 +0100

    Fix issue with 1px of next photo showing on mobile view

--- src/components/PhotoCarousel.js
+++ src/components/PhotoCarousel.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
+import './PhotoCarousel.css';
 
 function PhotoCarousel({ images }) {
   return (
